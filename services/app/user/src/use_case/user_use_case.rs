00ec137f3b7f0c37a6e149d4a59f9c0afbeb7c74
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-10 03:08:38 +0900

    fix: RegisterUserUseCaseのregister_user関数の引数を修正

--- services/app/user/src/use_case/user_use_case.rs
+++ services/app/user/src/use_case/user_use_case.rs
@@ -5,5 +5,10 @@ use crate::use_case::user_dto::UserDTO;
 
 #[async_trait]
 pub trait RegisterUserUseCase: Send + Sync + 'static {
-    async fn register_user(&self, user_id: &str) -> Result<UserDTO, UseCaseError>;
+    async fn register_user(
+        &self,
+        user_id: String,
+        name: String,
+        email: String,
+    ) -> Result<UserDTO, UseCaseError>;
 }
