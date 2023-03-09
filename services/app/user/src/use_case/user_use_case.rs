f6982ec717d5a54bda0bed9d65521ddb281a5048
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-10 02:37:05 +0900

    ref: UserDTOの変更に伴い修正

--- services/app/user/src/use_case/user_use_case.rs
+++ services/app/user/src/use_case/user_use_case.rs
@@ -1,9 +1,9 @@
 use async_trait::async_trait;
 use common::error::UseCaseError;
 
-use crate::use_case::user_dto::UserDto;
+use crate::use_case::user_dto::UserDTO;
 
 #[async_trait]
 pub trait RegisterUserUseCase: Send + Sync + 'static {
-    async fn register_user(&self, user_id: &str) -> Result<UserDto, UseCaseError>;
+    async fn register_user(&self, user_id: &str) -> Result<UserDTO, UseCaseError>;
 }
