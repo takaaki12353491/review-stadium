daaa24a1022903b3adac1881bfd194e85ab1ea3a
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-11 14:19:27 +0900

    fix: find_by_idでStringではなくIDを受け取るように修正

--- services/app/user/src/domain/user_repository.rs
+++ services/app/user/src/domain/user_repository.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use common::error::DomainError;
+use common::{error::DomainError, model::ID};
 use mockall::automock;
 
 use crate::domain::user::User;
@@ -8,5 +8,5 @@ use crate::domain::user::User;
 #[async_trait]
 pub trait UserRepository: Send + Sync + 'static {
     async fn create(&self, user: &User) -> Result<(), DomainError>;
-    async fn find_by_id(&self, id: &String) -> Result<Option<User>, DomainError>;
+    async fn find_by_id(&self, id: &ID) -> Result<Option<User>, DomainError>;
 }
