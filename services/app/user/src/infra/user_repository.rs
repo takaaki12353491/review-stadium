e42e3d02b68f1eebc3bec632f3974db176625310
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-12 01:51:54 +0900

    feat: user_repositoryを実装

--- services/app/user/src/infra/user_repository.rs
+++ services/app/user/src/infra/user_repository.rs
@@ -1,1 +1,70 @@
+use crate::domain::{user::User, user_repository::UserRepository};
+use async_trait::async_trait;
+use common::{error::DomainError, model::Model, model::ID};
+use sqlx::{PgConnection, PgPool};
 
+#[derive(sqlx::FromRow)]
+struct UserRow {
+    id: String,
+    user_id: String,
+    name: String,
+    email: String,
+}
+
+impl Into<User> for UserRow {
+    fn into(self) -> User {
+        User {
+            model: Model { id: self.id },
+            user_id: self.user_id,
+            name: self.name,
+            email: self.email,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PgUserRepository {
+    pool: PgPool,
+}
+
+impl PgUserRepository {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait]
+impl UserRepository for PgUserRepository {
+    async fn create(&self, user: &User) -> Result<(), DomainError> {
+        let mut conn = self.pool.acquire().await?;
+        let result = InternalUserRepository::create(user, &mut conn).await?;
+        Ok(result)
+    }
+
+    async fn find_by_id(&self, id: &ID) -> Result<Option<User>, DomainError> {
+        let mut conn = self.pool.acquire().await?;
+        let user = InternalUserRepository::find_by_id(id, &mut conn).await?;
+        Ok(user)
+    }
+}
+
+pub(super) struct InternalUserRepository {}
+
+impl InternalUserRepository {
+    pub(super) async fn create(user: &User, conn: &mut PgConnection) -> Result<(), DomainError> {
+        sqlx::query("INSERT INTO bookshelf_user (id) VALUES ($1)")
+            .bind(user.model.id.as_str())
+            .execute(conn)
+            .await?;
+        Ok(())
+    }
+
+    async fn find_by_id(id: &ID, conn: &mut PgConnection) -> Result<Option<User>, DomainError> {
+        let row: Option<UserRow> = sqlx::query_as("SELECT * FROM bookshelf_user WHERE id = $1")
+            .bind(id.as_str())
+            .fetch_optional(conn)
+            .await?;
+
+        row.map(|row| row.into()).transpose()
+    }
+}
