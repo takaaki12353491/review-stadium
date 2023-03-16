047fa242017080b60ca62b09fdc851c0f5e2c02c
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-17 06:26:17 +0900

    feat: GraphQLサーバーのサンプルをコピー

--- services/app/user/src/main.rs
+++ services/app/user/src/main.rs
@@ -3,6 +3,80 @@ mod domain;
 mod infra;
 mod use_case;
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::web::Data;
+use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{EmptySubscription, Object, Schema, SimpleObject};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+static SEQUENCE_ID: Lazy<Mutex<usize>> = Lazy::new(|| Mutex::new(0));
+static PHOTOS: Lazy<Mutex<Vec<Photo>>> = Lazy::new(|| Mutex::new(vec![]));
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn total_photos(&self) -> usize {
+        PHOTOS.lock().unwrap().len()
+    }
+
+    async fn all_photos(&self) -> Vec<Photo> {
+        PHOTOS.lock().unwrap().clone()
+    }
+}
+
+#[derive(SimpleObject, Clone)]
+struct Photo {
+    id: usize,
+    name: String,
+    description: String,
+}
+
+struct Mutation;
+
+#[Object]
+impl Mutation {
+    async fn post_photo(&self, name: String, description: String) -> Photo {
+        let mut id = SEQUENCE_ID.lock().unwrap();
+        *id += 1;
+        let photo = Photo {
+            id: *id,
+            name,
+            description,
+        };
+        PHOTOS.lock().unwrap().push(photo.clone());
+        photo
+    }
+}
+
+type ApiSchema = Schema<Query, Mutation, EmptySubscription>;
+
+async fn index(schema: web::Data<ApiSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_playground() -> Result<HttpResponse> {
+    let source = playground_source(GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"));
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(source))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let schema = Schema::build(Query, Mutation, EmptySubscription).finish();
+
+    println!("Playground: http://localhost:8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(schema.clone()))
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(web::resource("/").guard(guard::Get()).to(index_playground))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
