00b8a907c065ba64afdf1f382d7ffa2b7ba3ea6d
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-18 07:26:51 +0900

    fix: UseCaseを１つにまとめるように修正

--- services/app/user/src/use_case/user_interactor.rs
+++ services/app/user/src/use_case/user_interactor.rs
@@ -1,24 +1,24 @@
-use super::user_use_case::RegisterUserUseCase;
+use super::user_use_case::UserUseCase;
 use crate::domain::{user::User, user_repository::UserRepository};
 use async_trait::async_trait;
 use common::error::UseCaseError;
 
-pub struct RegisterUserInteractor<UR> {
+pub struct UserInteractor<UR> {
     user_repository: UR,
 }
 
-impl<UR> RegisterUserInteractor<UR> {
+impl<UR> UserInteractor<UR> {
     pub fn new(user_repository: UR) -> Self {
         Self { user_repository }
     }
 }
 
 #[async_trait]
-impl<UR> RegisterUserUseCase for RegisterUserInteractor<UR>
+impl<UR> UserUseCase for UserInteractor<UR>
 where
     UR: UserRepository,
 {
-    async fn execute(
+    async fn register(
         &self,
         user_id: String,
         name: String,
--- services/app/user/src/use_case/user_use_case.rs
+++ services/app/user/src/use_case/user_use_case.rs
@@ -3,8 +3,8 @@ use async_trait::async_trait;
 use common::error::UseCaseError;
 
 #[async_trait]
-pub trait RegisterUserUseCase: Send + Sync + 'static {
-    async fn execute(
+pub trait UserUseCase: Send + Sync + 'static {
+    async fn register(
         &self,
         user_id: String,
         name: String,
