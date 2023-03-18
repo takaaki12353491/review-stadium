569ecfcbf20d3bf2665fcc599b61db58563364b8
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-19 05:36:17 +0900

    ref: ポートを分けるように修正

--- services/app/user/src/main.rs
+++ services/app/user/src/main.rs
@@ -55,7 +55,7 @@ async fn main() -> std::io::Result<()> {
             )
             .service(web::resource("/").guard(guard::Get()).to(index_playground))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
