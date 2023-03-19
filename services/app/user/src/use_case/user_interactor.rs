74e38b85adc9bb72509ba96de43866122ca99402
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-19 17:47:30 +0900

    test: registerのテストを作成

--- services/app/user/src/use_case/user_interactor.rs
+++ services/app/user/src/use_case/user_interactor.rs
@@ -35,3 +35,54 @@ where
         Ok(user)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::domain::user_repository::MockUserRepository;
+    use anyhow::Error;
+    use common::error::DomainError;
+    use mockall::predicate::always;
+
+    #[tokio::test]
+    async fn test_register_success() {
+        let mut mock_user_repository = MockUserRepository::new();
+        mock_user_repository
+            .expect_create()
+            .times(1)
+            .with(always())
+            .returning(|_| Ok(()));
+
+        let user_usecase = UserInteractor::new(mock_user_repository);
+        let res = user_usecase
+            .register(
+                String::from("user_id"),
+                String::from("name"),
+                String::from("sample@example.com"),
+            )
+            .await;
+
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_register_failed() {
+        let mut mock_user_repository = MockUserRepository::new();
+        mock_user_repository
+            .expect_create()
+            .times(1)
+            .with(always())
+            .returning(|_| Err(DomainError::RepositoryError(Error::msg("Database Error"))));
+
+        let user_usecase = UserInteractor::new(mock_user_repository);
+        let res = user_usecase
+            .register(
+                String::from("user_id"),
+                String::from("name"),
+                String::from("sample@example.com"),
+            )
+            .await;
+
+        assert!(res.is_err());
+    }
+}
