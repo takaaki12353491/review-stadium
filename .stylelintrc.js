fb78a576d1a53344565bed8c97d4677202420a2c
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-25 14:49:59 +0900

    chore: stylelint-config-recess-orderとstylelint-orderを追加

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: "stylelint-config-standard",
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+  ],
+  plugins: [
+    "stylelint-order"
+  ],
   rules: {
     'at-rule-no-unknown': null,
   }
