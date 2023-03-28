4d0d7725bf1ad32d788b7ba37e3fe08afa6a21a6
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-28 23:59:52 +0900

    chore: storybookにaliasの設定を追加

--- front/web/.storybook/main.js
+++ front/web/.storybook/main.js
@@ -1,6 +1,9 @@
+const path = require("path");
+const rootPath = path.resolve(__dirname, "../app/");
+
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../app/**/*.mdx", "../app/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -23,5 +26,9 @@ const config = {
     autodocs: "tag",
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.resolve.alias["@"] = rootPath;
+    return config;
+  },
 };
 export default config;
