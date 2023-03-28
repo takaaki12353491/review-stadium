6cc9d5db9ac100d2f7e50c4489b490d3c3100d72
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-29 00:39:00 +0900

    chore: storybookをnext/imageに対応させる設定を追加

--- front/web/.storybook/preview.js
+++ front/web/.storybook/preview.js
@@ -1,5 +1,13 @@
+import * as nextImage from "next/image";
 import { RouterContext } from "next/dist/shared/lib/router-context";
 import "../app/styles/globals.scss";
+
+Object.defineProperty(nextImage, "default", {
+  configurable: true,
+  value: (props) => {
+    return <img {...props} />;
+  },
+});
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
