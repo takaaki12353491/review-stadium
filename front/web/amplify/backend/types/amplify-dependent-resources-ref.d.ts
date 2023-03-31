e71b41b63156c127315b60d4f980072e87c792ea
takaaki12353491 <takaaki12353491@gmail.com>
2023-04-01 06:04:21 +0900

    chore: amplifyに認証情報を追加

--- front/web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ front/web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "revistae1866733": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
