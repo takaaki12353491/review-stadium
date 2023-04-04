cab239a0974c2ad24557f9ad341673ad8eb91a3c
takaaki12353491 <takaaki12353491@gmail.com>
2023-04-04 18:45:47 +0900

    feat: Providerでamplifyの設定を行うように修正

--- front/web/app/Provider.tsx
+++ front/web/app/Provider.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { Amplify } from "aws-amplify";
+import awsExports from "@/app/aws-exports";
 import { ChakraProvider } from "@chakra-ui/react";
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return <ChakraProvider>{children}</ChakraProvider>;
