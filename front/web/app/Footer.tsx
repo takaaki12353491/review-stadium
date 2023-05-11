509df99b01e906a68a1679f76e2b966c62d6e661
takaaki12353491 <takaaki12353491@gmail.com>
2023-05-12 03:54:17 +0900

    style: footerの色を修正

--- front/web/app/Footer.tsx
+++ front/web/app/Footer.tsx
@@ -2,7 +2,7 @@ import { Container, Box, Text } from "@/app/components/chakra-ui";
 
 export default function Footer() {
   return (
-    <Box bg="gray.50" color="gray.700" as="footer">
+    <Box bg="white" as="footer">
       <Container maxW="5xl" py={4}>
         <Text as="small">© 2023 takaaki12353491</Text>
       </Container>
