c64f3d6618c17c9144220abe5e87d1ac447a8abb
takaaki12353491 <takaaki12353491@gmail.com>
2023-05-12 07:36:12 +0900

    fix: ビルドできるようにmypageをクリア

--- front/web/app/mypage/page.tsx
+++ front/web/app/mypage/page.tsx
@@ -1,12 +1,3 @@
-import { Authenticator } from "@aws-amplify/ui-react";
-import "../styles/globals.css";
-import "antd/dist/antd.dark.css";
-import "@aws-amplify/ui-react/styles.css";
-
 export default function Page() {
-  return (
-    <Authenticator.Provider>
-      <div>mypage</div>
-    </Authenticator.Provider>
-  );
+  return <></>;
 }
