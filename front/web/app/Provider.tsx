a847da0ff8d1ec597dd5be6358bc00fddba91170
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-15 05:52:44 +0900

    feat: SessionProviderをProviderに追加

--- front/web/app/Provider.tsx
+++ front/web/app/Provider.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/app/_styles/theme";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <SessionProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </SessionProvider>
+  );
 }
