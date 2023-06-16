644426fb36b0be7d37589b7c68e527323fe562f0
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-16 17:09:21 +0900

    fix: NextAuthの認証APIの設定をoptionsから首都クス領に設定

--- front/web/app/api/auth/[...nextauth]/route.ts
+++ front/web/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,6 @@
+import { options } from "@/app/options";
 import NextAuth from "next-auth";
-import CognitoProvider from "next-auth/providers/cognito";
 
-const handler = NextAuth({
-  providers: [
-    CognitoProvider({
-      clientId: process.env.COGNITO_CLIENT_ID,
-      clientSecret: process.env.COGNITO_CLIENT_SECRET,
-      issuer: process.env.COGNITO_ISSUER,
-    }),
-  ],
-});
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
