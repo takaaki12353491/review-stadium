97cc189449777c7a3d8d6ef844b547e2db257489
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-17 10:56:01 +0900

    ref: 不要な呼び出しを削除

--- front/web/app/_options/next-auth.ts
+++ front/web/app/_options/next-auth.ts
@@ -1,6 +1,5 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
   debug: true,
