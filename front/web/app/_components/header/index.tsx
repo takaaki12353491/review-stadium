890397307e088868b7da9f370ff90329a8bfd927
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-17 10:36:33 +0900

    fix: Headerから不要なNavLinkを削除

--- front/web/app/_components/header/index.tsx
+++ front/web/app/_components/header/index.tsx
@@ -1,41 +1,15 @@
 "use client";
 
-import { ReactNode } from "react";
 import {
   Box,
   Flex,
   HStack,
-  Link,
-  IconButton,
-  useDisclosure,
   useColorModeValue,
-  Stack,
-  HamburgerIcon,
-  CloseIcon,
 } from "@/app/_components/chakra-ui";
 import AuthArea from "./AuthArea";
 import NextLink from "next/link";
 
-const Links = ["Dashboard", "Projects", "Team"];
-
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={"md"}
-    _hover={{
-      textDecoration: "none",
-      bg: useColorModeValue("gray.200", "gray.700"),
-    }}
-    href={"#"}
-  >
-    {children}
-  </Link>
-);
-
 export default function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <Box
       as={"header"}
@@ -47,33 +21,11 @@ export default function Header() {
       bg={useColorModeValue("white", "gray.800")}
     >
       <Flex h={16} alignItems={"center"} justifyContent={"space-around"}>
-        <IconButton
-          size={"md"}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={"Open Menu"}
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-        />
         <HStack spacing={8} alignItems={"center"}>
           <NextLink href="/">Logo</NextLink>
-          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
-            {Links.map((link) => (
-              <NavLink key={link}>{link}</NavLink>
-            ))}
-          </HStack>
         </HStack>
         <AuthArea />
       </Flex>
-
-      {isOpen ? (
-        <Box pb={4} display={{ md: "none" }}>
-          <Stack as={"nav"} spacing={4}>
-            {Links.map((link) => (
-              <NavLink key={link}>{link}</NavLink>
-            ))}
-          </Stack>
-        </Box>
-      ) : null}
     </Box>
   );
 }
