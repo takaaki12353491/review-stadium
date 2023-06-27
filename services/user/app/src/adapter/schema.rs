2cc348a291a48caf289dbb00af19fb4f854d3bcb
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-28 02:53:04 +0900

    fix: Modelを修正したので関連部分を修正

--- services/user/app/src/adapter/schema.rs
+++ services/user/app/src/adapter/schema.rs
@@ -12,7 +12,7 @@ pub struct UserObject {
 impl From<User> for UserObject {
     fn from(user: User) -> Self {
         Self {
-            id: user.model.id.into_string(),
+            id: user.model.id.as_str(),
             id_name: user.id_name,
             name: user.name,
             email: user.email.unwrap_or_default(),
