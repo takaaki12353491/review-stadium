a9f186ac1c395a657adefe53002feb8cdb5770b4
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-28 02:53:46 +0900

    fix: user_repositoryのエラーハンドリングを追加

--- services/user/app/src/infra/user_repository.rs
+++ services/user/app/src/infra/user_repository.rs
@@ -1,18 +1,23 @@
 use crate::domain::{user::User, user_repository::UserRepository};
 use async_trait::async_trait;
+use chrono::{DateTime, NaiveDateTime, Utc};
 use common::{
     error::DomainError,
     model::{Model, ID},
     string::StringExt,
 };
 use sqlx::{PgConnection, PgPool};
+use tracing::*;
+use uuid::Uuid;
 
 #[derive(sqlx::FromRow)]
 struct UserRow {
-    id: String,
+    id: Uuid,
     id_name: String,
     name: String,
     email: String,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
 }
 
 impl From<UserRow> for User {
@@ -20,6 +25,8 @@ impl From<UserRow> for User {
         User {
             model: Model {
                 id: ID::from(row.id),
+                created_at: DateTime::<Utc>::from_utc(row.created_at, Utc),
+                updated_at: DateTime::<Utc>::from_utc(row.updated_at, Utc),
             },
             id_name: row.id_name,
             name: row.name,
@@ -69,26 +76,38 @@ impl InternalUserRepository {
         email: &str,
         conn: &mut PgConnection,
     ) -> Result<User, DomainError> {
-        let user_row: UserRow = sqlx::query_as(
-            "INSERT INTO users (id_name, name, email) VALUES ($1, $2, $3) RETURNING *",
+        let row_result = sqlx::query_as::<_, UserRow>(
+            "INSERT INTO \"user\".\"users\" (id_name, name, email) VALUES ($1, $2, $3) RETURNING *",
         )
         .bind(id_name)
         .bind(name)
         .bind(email)
         .fetch_one(conn)
-        .await?;
-        Ok(User::from(user_row))
+        .await;
+
+        match row_result {
+            Ok(user_row) => Ok(User::from(user_row)),
+            Err(e) => {
+                error!("error: {:?}", e);
+                Err(DomainError::from(e))
+            }
+        }
     }
 
     async fn find_by_id(id: &ID, conn: &mut PgConnection) -> Result<Option<User>, DomainError> {
-        let row: Option<UserRow> = sqlx::query_as("SELECT * FROM users WHERE id = $1")
-            .bind(id.as_str())
-            .fetch_optional(conn)
-            .await?;
+        let row_result =
+            sqlx::query_as::<_, UserRow>("SELECT * FROM \"user\".\"users\" WHERE id = $1")
+                .bind(id.as_str())
+                .fetch_optional(conn)
+                .await;
 
-        match row {
-            Some(user_row) => Ok(Some(user_row.into())),
-            None => Ok(None),
+        match row_result {
+            Ok(Some(user_row)) => Ok(Some(user_row.into())),
+            Ok(None) => Ok(None),
+            Err(e) => {
+                error!("error: {:?}", e);
+                Err(DomainError::from(e))
+            }
         }
     }
 
@@ -96,14 +115,19 @@ impl InternalUserRepository {
         id_name: &str,
         conn: &mut PgConnection,
     ) -> Result<Option<User>, DomainError> {
-        let row: Option<UserRow> = sqlx::query_as("SELECT * FROM users WHERE id_name = $1")
-            .bind(id_name)
-            .fetch_optional(conn)
-            .await?;
+        let row_result =
+            sqlx::query_as::<_, UserRow>("SELECT * FROM \"user\".\"users\" WHERE id_name = $1")
+                .bind(id_name)
+                .fetch_optional(conn)
+                .await;
 
-        match row {
-            Some(user_row) => Ok(Some(user_row.into())),
-            None => Ok(None),
+        match row_result {
+            Ok(Some(user_row)) => Ok(Some(user_row.into())),
+            Ok(None) => Ok(None),
+            Err(e) => {
+                error!("error: {:?}", e);
+                Err(DomainError::from(e))
+            }
         }
     }
 }
