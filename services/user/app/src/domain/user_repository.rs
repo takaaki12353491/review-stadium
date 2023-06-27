8d70abb27d3dceab014220c9a33bdebb37cb59df
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-27 16:49:38 +0900

    feat: UserRepositoryにDebugを継承

--- services/user/app/src/domain/user_repository.rs
+++ services/user/app/src/domain/user_repository.rs
@@ -2,10 +2,11 @@ use super::user::User;
 use async_trait::async_trait;
 use common::{error::DomainError, model::ID};
 use mockall::automock;
+use std::fmt::Debug;
 
 #[automock]
 #[async_trait]
-pub trait UserRepository: Send + Sync + 'static {
+pub trait UserRepository: Send + Sync + Debug + 'static {
     async fn create(&self, id_name: &str, name: &str, email: &str) -> Result<User, DomainError>;
     async fn find_by_id(&self, id: &ID) -> Result<Option<User>, DomainError>;
     async fn find_by_id_name(&self, id_name: &str) -> Result<Option<User>, DomainError>;
