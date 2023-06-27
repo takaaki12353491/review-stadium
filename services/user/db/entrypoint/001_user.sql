eef15a795645c552c432707c30b0b2f98fb09e5a
takaaki12353491 <takaaki12353491@gmail.com>
2023-06-27 10:32:49 +0900

    fix: userのsqlを修正

--- services/user/db/entrypoint/001_user.sql
+++ services/user/db/entrypoint/001_user.sql
@@ -4,9 +4,9 @@ CREATE SCHEMA IF NOT EXISTS "user";
 
 CREATE TABLE IF NOT EXISTS "user"."users" (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "id_name" VARCHAR(20) NOT NULL,
+  "id_name" VARCHAR(20) UNIQUE NOT NULL,
   "name" VARCHAR(255) NOT NULL,
-  "email" VARCHAR(255) NOT NULL UNIQUE,
+  "email" VARCHAR(255) UNIQUE NOT NULL,
   "email_verified" TIMESTAMP,
   "image" VARCHAR(255),
   "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS "user"."accounts" (
   "type" VARCHAR(255) NOT NULL,
   "provider" VARCHAR(255) NOT NULL,
   "provider_account_id" VARCHAR(255) NOT NULL,
-  "refresh_token" VARCHAR(255),
-  "access_token" VARCHAR(255),
+  "refresh_token" VARCHAR(255) UNIQUE,
+  "access_token" VARCHAR(255) UNIQUE,
   "expires_at" INTEGER,
   "token_type" VARCHAR(255),
   "scope" VARCHAR(255),
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS "user"."accounts" (
 CREATE TABLE IF NOT EXISTS "user"."sessions" (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   "expires" TIMESTAMP NOT NULL,
-  "session_token" VARCHAR(255) NOT NULL,
+  "session_token" VARCHAR(255) NOT NULL UNIQUE,
   "user_id" UUID NOT NULL REFERENCES "user"."users"("id"),
   "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -42,15 +42,11 @@ CREATE TABLE IF NOT EXISTS "user"."sessions" (
 CREATE TABLE IF NOT EXISTS "user"."verification_tokens" (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   "identifier" VARCHAR(255) NOT NULL,
-  "token" VARCHAR(255) NOT NULL,
+  "token" VARCHAR(255) NOT NULL UNIQUE,
   "expire" TIMESTAMP NOT NULL,
   "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS "email" ON "user"."users"("email");
 CREATE INDEX IF NOT EXISTS "user_id" ON "user"."accounts"("user_id");
 CREATE INDEX IF NOT EXISTS "provider_account_id" ON "user"."accounts"("provider_account_id");
-CREATE UNIQUE INDEX IF NOT EXISTS "access_token" ON "user"."accounts"("access_token");
-CREATE UNIQUE INDEX IF NOT EXISTS "session_token" ON "user"."sessions"("session_token");
-CREATE UNIQUE INDEX IF NOT EXISTS "token" ON "user"."verification_tokens"("token");
